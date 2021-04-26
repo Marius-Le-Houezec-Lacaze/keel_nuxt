9c48fa4d7787c7687681e5f23810abaf1a88087d
MariusLHL <mariuslhl@gmail.com>
2021-04-26 22:54:11 +0200

    skeleton login page functional

---
+++ pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+    <form  method="post" @submit.prevent="login">
+        <input type="text" v-model="mail">
+        <input type="text"  v-model="password">
+        <button type="submit" > Lgin </button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "login",
+    data(){
+        return{
+            mail: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        login(){
+					try{
+            this.$axios.post('/api/login',{
+                headers: { "Content-Type": "application/json" },
+                body:{
+                    	mail: this.mail,
+                      password: this.password
+										},
+								credentials: 'include'
+            }).then(response => console.log(response.data))
+						this.$router.push('/')
+					} catch (e) {
+						console.log(e)
+					}
+        }	
+    }
+
+}
+</script>
+
+<style>
+
+</style>
